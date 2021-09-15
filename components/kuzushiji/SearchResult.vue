410a2bc89c32a433c852a85c25b682b7d6c89b1f
toyjack <toyjack@gmail.com>
2021-09-15 19:45:12 +0900

    feat: add radical filter

--- components/kuzushiji/SearchResult.vue
+++ components/kuzushiji/SearchResult.vue
@@ -27,6 +27,18 @@
           rounded
         ></v-select>
       </v-col>
+      <v-col cols="12" md="3">
+        <v-select
+          dense
+          single-line
+          hide-details
+          v-model="radical"
+          :items="radicalItems"
+          outlined
+          rounded
+        ></v-select>
+      </v-col>
+
       <v-col cols="12" md="9" class="text-right">
         <v-btn
           v-for="(option, key) in layouts"
@@ -84,6 +96,9 @@ export default class ShareButtons extends Vue {
 
   layout_: string = 'grid'
 
+  radicalItems: any = ['一','丨','丶','丿','乙','亅','二','亠','人','儿','入','八','冂','冖','冫','几','凵','刀','力','勹','匕','匚','匸','十','卜','卩','厂','厶','又','口','囗','土','士','夂','夊','夕','大','女','子','宀','寸','小','尢','尸','屮','山','巛','工','己','巾','干','幺','广','廴','廾','弋','弓','彐','彡','彳','心','戈','戶','手','支','攴','文','斗','斤','方','无','日','曰','月','木','欠','止','歹','殳','毋','比','毛','氏','气','水','火','爪','父','爻','爿','片','牙','牛','犬','玄','玉','瓜','瓦','甘','生','用','田','疋','疒','癶','白','皮','皿','目','矛','矢','石','示','禸','禾','穴','立','竹','米','糸','缶','网','羊','羽','老','而','耒','耳','聿','肉','臣','自','至','臼','舌','舛','舟','艮','色','艸','虍','虫','血','行','衣','襾','見','角','言','谷','豆','豕','豸','貝','赤','走','足','身','車','辛','辰','辵','邑','酉','釆','里','金','長','門','阜','隶','隹','雨','靑','非','面','革','韋','韭','音','頁','風','飛','食','首','香','馬','骨','高','髟','鬥','鬯','鬲','鬼','魚','鳥','鹵','鹿','麥','麻','黃','黍','黑','黹','黽','鼎','鼓','鼠','鼻','齊','齒','龍','龜','龠',]
+  radical: string = ''
+
   sortItems: any = [
     {
       text: this.$t('出現回数'),
@@ -102,6 +117,7 @@ export default class ShareButtons extends Vue {
     const items = this.items
 
     const key = this.sort
+    const key2 = this.radical
 
     if (key === 'size') {
       items.sort(function (a: any, b: any) {
@@ -115,6 +131,17 @@ export default class ShareButtons extends Vue {
         if (a.sort > b.sort) return 1
         return 0
       })
+    }
+
+    if (key2 != '') {
+      let filterdByRadical = []
+      console.log(key2)
+      for (const item of items) {
+        if (key2 === item.radical) {
+          filterdByRadical.push(item)
+        }
+      }
+      return filterdByRadical
     }
 
     if (!search) {
